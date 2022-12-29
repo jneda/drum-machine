5e25a33c3464d1fc0700fe73e81faba72df3a35d
jneda <julien.dane@gmail.com>
2022-12-29 10:40:21 +0100

    feat: add Bootstrap

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 class DrumPad extends React.Component {
   render() {
-    return (
-      <div className='drum-pad'>
-        {this.props.value}
-      </div>
-    );
+    return <div className="drum-pad col">
+      <button className='btn btn-primary w-100'>{this.props.value}</button>
+    </div>;
   }
 }
 
@@ -15,22 +13,17 @@ class PadBank extends React.Component {
       const padRow = [];
       for (let colIndex = 0; colIndex < 3; colIndex++) {
         const padIndex = rowIndex * 3 + colIndex;
-        padRow.push(
-          <DrumPad
-            key={padIndex}
-            value={padIndex}
-          />
-        );
+        padRow.push(<DrumPad key={padIndex} value={padIndex} />);
       }
       pads.push(
-        <div className='pad-row' key={rowIndex}>
+        <div className="pad-row row g-1 my-1" key={rowIndex}>
           {padRow}
         </div>
       );
     }
 
     return (
-      <div id='pad-bank'>
+      <div id="pad-bank" className="col">
         {pads}
       </div>
     );
@@ -40,7 +33,7 @@ class PadBank extends React.Component {
 class Controls extends React.Component {
   render() {
     return (
-      <div id='display'>
+      <div id="display" className="col">
         {this.props.value}
       </div>
     );
@@ -49,15 +42,17 @@ class Controls extends React.Component {
 
 class DrumMachine extends React.Component {
   render() {
-    const currentSound = 'Sound X';
+    const currentSound = "Sound X";
     return (
-      <div id='drum-machine'>
-        <PadBank />
-        <Controls value={currentSound} />
+      <div id="drum-machine" className="container">
+        <div className='row'>
+          <PadBank />
+          <Controls value={currentSound} />
+        </div>
       </div>
     );
   }
 }
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(<DrumMachine />);
